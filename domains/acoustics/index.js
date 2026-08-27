d7ed3eb627945c362afb1790cadd7bd831033924
Aki Hirvilammi <aki.hirvilammi@gmail.com>
2026-08-27 18:41:12 +0300

    Export shared field view range coordinator

--- domains/acoustics/index.js
+++ domains/acoustics/index.js
@@ -3,3 +3,4 @@ export { FrequencyRangeController } from './frequency-range-controller.js';
 export { SpeakerNode } from './speaker-node.js';
 export { SampledFieldPlane } from './sampled-field-plane.js';
 export { OrthogonalFieldSlices } from './orthogonal-field-slices.js';
+export { FieldViewRangeCoordinator } from './field-view-range-coordinator.js';
